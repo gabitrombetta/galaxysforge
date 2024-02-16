652dc61b342a8b7a487d18960739b8c9019f358f
Gabriela <gabrielatrombetta20@gmail.com>
2024-02-16 17:56:48 -0300

    att

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Ativar links do Menu
 const links = document.querySelectorAll('.header-menu a')
 
 function ativarLink(link){
@@ -10,3 +11,56 @@ function ativarLink(link){
 }
 
 links.forEach(ativarLink)
+
+// Ativar itens do orçamento
+
+const parametros = new URLSearchParams(location.search)
+
+function ativarProduto(parametro){
+    const element = document.getElementById(parametro)
+    if(element) {
+        element.checked = true
+    }
+}
+
+parametros.forEach(ativarProduto)
+
+// Perguntas frequentes
+
+const perguntas = document.querySelectorAll('.perguntas button')
+
+function ativarPergunta(event){
+    const pergunta = event.currentTarget
+    const controls = pergunta.getAttribute('aria-controls')
+    const resposta = document.getElementById(controls)
+
+    resposta.classList.toggle("ativa")
+    const ativa = resposta.classList.contains('ativa')
+    pergunta.setAttribute('aria-expanded', 'true')
+}
+
+function eventosPerguntas(pergunta){
+    pergunta.addEventListener('click', ativarPergunta)
+}
+
+perguntas.forEach(eventosPerguntas)
+
+// Galeria de imagens
+
+const galeria = document.querySelectorAll('.sabre-imagens img')
+const galeriaContainer = document.querySelector('.sabre-imagens')
+
+function trocarImagem(event){
+    const img = event.currentTarget
+    const media = matchMedia('(min-width: 1000px)').matches
+    console.log(media)
+    if(media){
+        galeriaContainer.prepend(img)
+    }
+}
+
+function eventosGaleria(img){
+    img.addEventListener('click', trocarImagem)
+}
+
+galeria.forEach(eventosGaleria)
